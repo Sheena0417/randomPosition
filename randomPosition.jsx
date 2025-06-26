d0b2b3ff83fbb17d189a00ec7ffdc590e675401e
Sheena0417 <shiina@yogo.network>
2025-06-26 16:22:57 +0900

    add script UI

--- randomPosition.jsx
+++ randomPosition.jsx
@@ -1,64 +1,72 @@
-function random(min, max) {
-    return min + (max - min) * Math.random();
-}
+(function (thisObj) {
+    function buildUI(thisObj) {
+        var win = (thisObj instanceof Panel)
+            ? thisObj
+            : new Window("palette", "Random Z Position", undefined, { resizeable: false });
 
-function showRandomPositionUI() {
-    var win = new Window("palette", "ランダム位置設定", undefined);
-    win.orientation = "column";
-    win.alignChildren = "left";
+        if (win !== null) {
+            win.orientation = "column";
+            win.alignChildren = ["fill", "top"];
 
-    win.add("statictext", undefined, "Z軸 最小値:");
-    var minZInput = win.add("edittext", undefined, "-500");
-    minZInput.characters = 10;
+            // === Z範囲入力 ===
+            var zGroup = win.add("group");
+            zGroup.orientation = "row";
+            zGroup.add("statictext", undefined, "Min Z:");
+            var minZ = zGroup.add("edittext", undefined, "-500");
+            minZ.characters = 6;
+            zGroup.add("statictext", undefined, "Max Z:");
+            var maxZ = zGroup.add("edittext", undefined, "500");
+            maxZ.characters = 6;
 
-    win.add("statictext", undefined, "Z軸 最大値:");
-    var maxZInput = win.add("edittext", undefined, "500");
-    maxZInput.characters = 10;
+            // === 実行ボタン ===
+            var runBtn = win.add("button", undefined, "実行");
+            runBtn.alignment = "center";
 
-    var execBtn = win.add("button", undefined, "実行");
+            runBtn.onClick = function () {
+                var min = parseFloat(minZ.text);
+                var max = parseFloat(maxZ.text);
 
-    execBtn.onClick = function () {
-        var minZ = parseFloat(minZInput.text);
-        var maxZ = parseFloat(maxZInput.text);
-        if (isNaN(minZ) || isNaN(maxZ)) {
-            alert("数値を正しく入力してください。");
-            return;
+                if (isNaN(min) || isNaN(max)) {
+                    alert("数値を正しく入力してください。");
+                    return;
+                }
+
+                var comp = app.project.activeItem;
+                if (!(comp instanceof CompItem)) {
+                    alert("コンポジションを開いてください。");
+                    return;
+                }
+
+                var layers = comp.selectedLayers;
+                if (layers.length === 0) {
+                    alert("レイヤーを選択してください。");
+                    return;
+                }
+
+                app.beginUndoGroup("Random Z Position");
+
+                for (var i = 0; i < layers.length; i++) {
+                    var layer = layers[i];
+                    var pos = layer.property("Position").value;
+                    if (!layer.threeDLayer) {
+                        layer.threeDLayer = true;
+                    }
+                    var randZ = Math.random() * (max - min) + min;
+                    layer.property("Position").setValue([pos[0], pos[1], randZ]);
+                }
+
+                app.endUndoGroup();
+            };
+
+            win.layout.layout(true);
         }
 
-        applyRandomPosition(minZ, maxZ);
-    };
-
-    win.center();
-    win.show();
-}
-
-function applyRandomPosition(minZ, maxZ) {
-    var comp = app.project.activeItem;
-    if (comp && comp instanceof CompItem) {
-        var selectedLayers = comp.selectedLayers;
-        var compWidth = comp.width;
-        var compHeight = comp.height;
-
-        app.beginUndoGroup("Randomize Position with UI");
-
-        for (var i = 0; i < selectedLayers.length; i++) {
-            var layer = selectedLayers[i];
-            var x = random(0, compWidth);
-            var y = random(0, compHeight);
-
-            if (layer.threeDLayer) {
-                var z = random(minZ, maxZ);
-                layer.property("Position").setValue([x, y, z]);
-            } else {
-                layer.property("Position").setValue([x, y]);
-            }
-        }
-
-        app.endUndoGroup();
-    } else {
-        alert("コンポジションが選択されていません。");
+        return win;
     }
-}
 
-// スクリプト実行時にUIを表示
-showRandomPositionUI();
+    var myPanel = buildUI(thisObj);
+    if (myPanel instanceof Window) {
+        myPanel.center();
+        myPanel.show();
+    }
+})(this);
