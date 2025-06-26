ac9d5816af57ce79c4fb2823bc4dc8947052bc48
Sheena0417 <shiina@yogo.network>
2025-06-26 16:13:18 +0900

    Custom Z Position

--- randomPosition.jsx
+++ randomPosition.jsx
@@ -2,32 +2,63 @@ function random(min, max) {
     return min + (max - min) * Math.random();
 }
 
-var comp = app.project.activeItem;
-if (comp && comp instanceof CompItem) {
-    var selectedLayers = comp.selectedLayers;
-    var compWidth = comp.width;
-    var compHeight = comp.height;
+function showRandomPositionUI() {
+    var win = new Window("palette", "ランダム位置設定", undefined);
+    win.orientation = "column";
+    win.alignChildren = "left";
 
-    app.beginUndoGroup("Randomize X and Y");
+    win.add("statictext", undefined, "Z軸 最小値:");
+    var minZInput = win.add("edittext", undefined, "-500");
+    minZInput.characters = 10;
 
-    for (var i = 0; i < selectedLayers.length; i++) {
-        var layer = selectedLayers[i];
+    win.add("statictext", undefined, "Z軸 最大値:");
+    var maxZInput = win.add("edittext", undefined, "500");
+    maxZInput.characters = 10;
 
-        // 元のPosition値を取得
-        var pos = layer.property("Position").value;
+    var execBtn = win.add("button", undefined, "実行");
 
-        // 新しいx, yをコンポサイズに合わせて生成
-        var x = random(0, compWidth);
-        var y = random(0, compHeight);
-
-        // 3Dレイヤーならzを保持、2Dレイヤーなら2次元でセット
-        if (layer.threeDLayer) {
-            var z = pos[2]; // zを保持
-            layer.property("Position").setValue([x, y, z]);
-        } else {
-            layer.property("Position").setValue([x, y]);
+    execBtn.onClick = function () {
+        var minZ = parseFloat(minZInput.text);
+        var maxZ = parseFloat(maxZInput.text);
+        if (isNaN(minZ) || isNaN(maxZ)) {
+            alert("数値を正しく入力してください。");
+            return;
         }
-    }
 
-    app.endUndoGroup();
+        applyRandomPosition(minZ, maxZ);
+    };
+
+    win.center();
+    win.show();
 }
+
+function applyRandomPosition(minZ, maxZ) {
+    var comp = app.project.activeItem;
+    if (comp && comp instanceof CompItem) {
+        var selectedLayers = comp.selectedLayers;
+        var compWidth = comp.width;
+        var compHeight = comp.height;
+
+        app.beginUndoGroup("Randomize Position with UI");
+
+        for (var i = 0; i < selectedLayers.length; i++) {
+            var layer = selectedLayers[i];
+            var x = random(0, compWidth);
+            var y = random(0, compHeight);
+
+            if (layer.threeDLayer) {
+                var z = random(minZ, maxZ);
+                layer.property("Position").setValue([x, y, z]);
+            } else {
+                layer.property("Position").setValue([x, y]);
+            }
+        }
+
+        app.endUndoGroup();
+    } else {
+        alert("コンポジションが選択されていません。");
+    }
+}
+
+// スクリプト実行時にUIを表示
+showRandomPositionUI();
